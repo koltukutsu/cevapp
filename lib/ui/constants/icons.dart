a904e491b685ec788ec2b87ec2208f1ad0b160f9
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2022-08-08 14:38:40 +0300

    problems are solved, need to be cautious about androidx passing processing

--- lib/ui/constants/icons.dart
+++ lib/ui/constants/icons.dart
@@ -2,6 +2,7 @@
 class AppPaths {
   static const String acceptIconPath = "assets/images/colorful_accept.png";
   static const String pauseIconPath = "assets/images/colorful_pause.png";
+  static const String continueIconPath = "assets/images/colorful_continue.png";
   static const String removetIconPath = "assets/images/colorful_reject_second.png";
 
   static const String shuffleIconPath = "assets/images/shuffle_second.png";
