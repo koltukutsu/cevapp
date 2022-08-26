355bd5afe43cfe607f3698e8de125f63a366c020
Mehmet Semih BABACAN <l6918040@std.yildiz.edu.tr>
2022-08-26 19:50:27 +0300

    SPLASH_SCREEN[DONE] Splash screen is added yet need to adjust the sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:cevapp/ui/main_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   runApp(const MainApp());
+  FlutterNativeSplash.remove();
 }
