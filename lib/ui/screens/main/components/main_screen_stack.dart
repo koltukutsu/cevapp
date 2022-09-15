1a78a2d18fe994f72da7aeaf1e6879ce89f240fd
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2022-09-15 22:39:19 +0300

    Sliding_panel [upgrade]: prev sliding panel is removed

--- lib/ui/screens/main/components/main_screen_stack.dart
+++ lib/ui/screens/main/components/main_screen_stack.dart
@@ -15,32 +15,6 @@ class MainScreenStack extends StatefulWidget {
 class _MainScreenStackState extends State<MainScreenStack> {
   @override
   Widget build(BuildContext context) {
-    // return Stack(children: [
-    //   const MainScreenBody(),
-    //   if (MediaQuery.of(context).viewInsets.bottom == 0)
-    //     GestureDetector(
-    //         onHorizontalDragUpdate: (details) {
-    //           showModalBottomSheet(
-    //               isScrollControlled: true,
-    //               elevation: 8,
-    //               context: context,
-    //               shape: const RoundedRectangleBorder(
-    //                 borderRadius: BorderRadius.vertical(
-    //                   top: Radius.circular(60.0),
-    //                 ),
-    //               ),
-    //               builder: (context) {
-    //                 return const Expanded(child: RecordsScreen());
-    //               });
-    //         },
-    //         child: Align(
-    //           alignment: Alignment.bottomCenter,
-    //           child: Flex(
-    //             direction: Axis.horizontal,
-    //             children: const [Expanded(child: SwipeDock())],
-    //           ),
-    //         ))
-    // ]);
     return SlidingUpPanel(
       maxHeight: MediaQuery.of(context).size.height *
           AppRatios.swipdeDockFullHeightRatio,
@@ -56,38 +30,3 @@ class _MainScreenStackState extends State<MainScreenStack> {
     );
   }
 }
-
-// Widget _floatingCollapsed(){
-//   return Container(
-//     decoration: BoxDecoration(
-//       color: Colors.blueGrey,
-//       borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
-//     ),
-//     margin: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
-//     child: Center(
-//       child: Text(
-//         "This is the collapsed Widget",
-//         style: TextStyle(color: Colors.white),
-//       ),
-//     ),
-//   );
-// }
-//
-// Widget _floatingPanel(){
-//   return Container(
-//     decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.all(Radius.circular(24.0)),
-//         boxShadow: [
-//           BoxShadow(
-//             blurRadius: 20.0,
-//             color: Colors.grey,
-//           ),
-//         ]
-//     ),
-//     margin: const EdgeInsets.all(24.0),
-//     child: Center(
-//       child: Text("This is the SlidingUpPanel when open"),
-//     ),
-//   );
-// }
